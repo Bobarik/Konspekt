bdcb0e0869f67fa452006ca5c4c77383808af6e7
Vladimir Skorobogatov <vlaskorobogatov@gmail.com>
2025-05-13 18:30:07 +0500

    - switched logging to Napier - swapped broadcast receiver with StateFlow

--- core/network/src/androidMain/kotlin/com/bobarik/konspekt/core/network/VpnTunnelServiceImpl.kt
+++ core/network/src/androidMain/kotlin/com/bobarik/konspekt/core/network/VpnTunnelServiceImpl.kt
@@ -16,18 +16,17 @@ import android.os.Build
 import android.os.IBinder
 import android.os.ParcelFileDescriptor
 import androidx.annotation.RequiresApi
-import com.bobarik.konspekt.core.network.models.MessageData
 import com.bobarik.konspekt.core.network.models.TunnelConfig
 import com.bobarik.konspekt.core.network.models.TunnelStatus
-import outline.NewClientResult
+import com.bobarik.konspekt.logging.Log
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import outline.Outline
 import outline.TCPAndUDPConnectivityResult
-import tun2socks.ConnectOutlineTunnelResult
 import tun2socks.Tun2socks
 import tun2socks.Tunnel
 import java.io.IOException
-import java.util.logging.Level
-import java.util.logging.Logger
 
 @RequiresApi(Build.VERSION_CODES.O)
 class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
@@ -39,8 +38,11 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
   private val tunnelStore: VpnTunnelStore by lazy { VpnTunnelStore(this) }
   private lateinit var notificationBuilder: Notification.Builder
 
+  private val _connectivityState = MutableStateFlow<TunnelStatus>(TunnelStatus.Invalid)
+  val connectivityState = _connectivityState.asStateFlow()
+
   override fun onBind(intent: Intent): IBinder? {
-    Log.info("Binding VPN service: $intent")
+    Log.i("Binding VPN service: $intent")
     if (intent.action == SERVICE_INTERFACE) return super.onBind(intent)
     if (intent.getBooleanExtra(VpnServiceStarter.AutoStartExtra, false)) startLastSuccessfulTunnel()
     return super.onBind(intent)
@@ -48,7 +50,7 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
 
   /** This is the entrypoint when started by the OS.  */
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-    Log.info("Starting VPN service: $intent")
+    Log.i("Starting VPN service: $intent")
     if (intent != null) {
       // VpnServiceStarter puts AUTOSTART_EXTRA in the intent when the service starts automatically.
       val startedByVpnStarter = intent.getBooleanExtra(VpnServiceStarter.AutoStartExtra, false)
@@ -59,27 +61,27 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
   }
 
   override fun onRevoke() {
-    Log.info("VPN revoked.")
-    broadcastVpnConnectivityChange(TunnelStatus.Disconnected)
+    Log.i("VPN revoked.")
+    _connectivityState.update { TunnelStatus.Disconnected }
     tearDownActiveTunnel()
   }
 
   override fun onDestroy() {
-    Log.info("Destroying VPN service.")
+    Log.i("Destroying VPN service.")
     tearDownActiveTunnel()
   }
 
   // Autostart
   private fun startLastSuccessfulTunnel() {
-    Log.info("Received an auto-connect request, loading last successful tunnel.")
+    Log.i("Received an auto-connect request, loading last successful tunnel.")
     val tunnel = tunnelStore.load()
     if (tunnel == null) {
-      Log.info("Last successful tunnel not found. User not connected at shutdown/install.")
+      Log.i("Last successful tunnel not found. User not connected at shutdown/install.")
       return
     }
     if (prepare(this) != null) {
       // We cannot prepare the VPN when running as a background service, as it requires UI.
-      Log.warning("VPN not prepared, aborting auto-connect.")
+      Log.w("VPN not prepared, aborting auto-connect.")
       return
     }
     try {
@@ -88,13 +90,13 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
       startForegroundWithNotification(tunnel.name)
       startTunnel(tunnel, true)
     } catch (e: Exception) {
-      Log.log(Level.SEVERE, "Failed to retrieve JSON tunnel data", e)
+      Log.e("Failed to retrieve JSON tunnel data", e)
     }
   }
 
   private fun storeActiveTunnel(config: TunnelConfig, isUdpSupported: Boolean) {
     tunnelStore.save(config)
-    tunnelStore.setTunnelStatus(TunnelStatus.Connected)
+    tunnelStore.setTunnelStatus(TunnelStatus.Connected(config.id))
     tunnelStore.setIsUdpSupported(isUdpSupported)
   }
 
@@ -114,7 +116,7 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
       // On close, the OS restores the routing and destroys the TUN device.
       tunFd?.close()
     } catch (_: IOException) {
-      Log.severe("Failed to close the VPN interface file descriptor.")
+      Log.e("Failed to close the VPN interface file descriptor.")
     } finally {
       tunFd = null
     }
@@ -128,7 +130,7 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
   @Synchronized
   private fun stopRemoteDevice() {
     remoteDevice?.let { device ->
-      if (device.isConnected()) device.disconnect()
+      if (device.isConnected) device.disconnect()
       remoteDevice = null
     }
   }
@@ -150,11 +152,12 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
   /** Updates the persistent notification to reflect the tunnel status.  */
   private fun updateNotification(status: TunnelStatus) {
     try {
+      _connectivityState.update { status }
       if (!::notificationBuilder.isInitialized) return
       val notification = notificationBuilder.setContentText(getString(status.labelId)).build()
       getSystemService(NotificationManager::class.java).notify(NotificationServiceId, notification)
     } catch (_: java.lang.Exception) {
-      Log.warning("Failed to update persistent notification")
+      Log.w("Failed to update persistent notification")
     }
   }
 
@@ -189,21 +192,21 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
     config: TunnelConfig,
     isAutoStart: Boolean,
   ) {
-    Log.info("Starting tunnel ${config.id} for server ${config.name}")
+    Log.i("Starting tunnel ${config.id} for server ${config.name}")
     // We check if the VPN is already running. This happens when a user connects to a server while
     // already connected to another one.
     // Instead of tearing down the VPN and starting from scratch, we just replace the remote device
     // and restart the traffic exchange.
-    val alreadyRunning = this.tunnelConfig != null && this.tunFd != null
+    val alreadyRunning = tunnelConfig != null && tunFd != null
     if (alreadyRunning) {
       // Broadcast the previous instance disconnect event before reassigning the tunnel config.
-      broadcastVpnConnectivityChange(TunnelStatus.Disconnected)
+      _connectivityState.update { TunnelStatus.Disconnected }
       stopForeground(STOP_FOREGROUND_REMOVE)
       try {
         // Stops the remote device; does not tear down the VPN to avoid leaking traffic.
         stopRemoteDevice()
       } catch (e: java.lang.Exception) {
-        Log.log(Level.SEVERE, "Failed to disconnect tunnel", e)
+        Log.e("Failed to disconnect tunnel", e)
       }
     } else {
       // Make sure we have a fully clean state.
@@ -211,13 +214,15 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
       tunFd = null
     }
 
-    val clientResult: NewClientResult = Outline.newClient(config.transportConfig)
+    val clientResult = Outline.newClient(config.transportConfig)
     if (clientResult.error != null) {
-      Log.log(Level.WARNING, "Failed to create Outline Client", clientResult.error)
+      Log.e(
+        message = "Failed to create Outline Client",
+        throwable = IllegalArgumentException(clientResult.error.message),
+      )
       tearDownActiveTunnel()
       throw error("Failed to create Outline Client")
     }
-    val client: outline.Client? = clientResult.client
 
     val remoteUdpForwardingEnabled: Boolean
     if (isAutoStart) {
@@ -227,8 +232,8 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
       try {
         // Do not perform connectivity checks when connecting on startup. We should avoid failing
         // the connection due to a network error, as network may not be ready.
-        val connResult: TCPAndUDPConnectivityResult = Outline.checkTCPAndUDPConnectivity(client)
-        Log.info("Go connectivity check result: $connResult")
+        val connResult: TCPAndUDPConnectivityResult = Outline.checkTCPAndUDPConnectivity(clientResult.client)
+        Log.i("Go connectivity check result: $connResult")
 
         if (connResult.tcpError != null) {
           tearDownActiveTunnel()
@@ -267,14 +272,14 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
         tunFd = builder.establish() ?: throw error("failed to establish the VPN")
         startNetworkConnectivityMonitor()
       } catch (e: Exception) {
-        Log.log(Level.SEVERE, "Failed to establish the VPN", e)
+        Log.e("Failed to establish the VPN", e)
         tearDownActiveTunnel()
         return
       }
     }
 
     // Start exchanging traffic between the local TUN device and the remote device.
-    val result: ConnectOutlineTunnelResult = Tun2socks.connectOutlineTunnel(tunFd?.fd?.toLong() ?: 0L, client, remoteUdpForwardingEnabled)
+    val result = Tun2socks.connectOutlineTunnel(tunFd?.fd?.toLong() ?: 0L, clientResult.client, remoteUdpForwardingEnabled)
     if (result.error != null) {
       tearDownActiveTunnel()
       throw error(result.error)
@@ -284,6 +289,13 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
     startForegroundWithNotification(config.name)
     storeActiveTunnel(config, remoteUdpForwardingEnabled)
   }
+
+  override fun stopTunnel(tunnelId: String) {
+    if (!isTunnelActive(tunnelId)) throw error("VPN profile is not active")
+    tearDownActiveTunnel()
+  }
+
+  override fun isTunnelActive(tunnelId: String): Boolean = tunnelConfig?.id == tunnelId
 
   private fun getReservedBypassSubnets(): List<Subnet> {
     val subnetStrings = resources.getStringArray(R.array.reserved_bypass_subnets)
@@ -296,22 +308,6 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
     val appInfo = packageManager.getApplicationInfo(packageName, 0)
     return packageManager.getApplicationLabel(appInfo).toString()
   }
-
-  // Broadcasts
-  /* Broadcast change in the VPN connectivity. */
-  private fun broadcastVpnConnectivityChange(status: TunnelStatus) {
-    if (tunnelConfig == null) {
-      Log.warning("Tunnel disconnected, not sending VPN connectivity broadcast")
-      return
-    }
-    val statusChange = Intent(StatusBroadcastKey)
-    statusChange.addCategory(packageName)
-    statusChange.setPackage(packageName)
-    statusChange.putExtra(MessageData.Payload.value, status.value)
-    statusChange.putExtra(MessageData.TunnelId.value, tunnelConfig!!.id) // TODO: extract nullables into its own config...
-    sendBroadcast(statusChange)
-  }
-
 
   private fun startNetworkConnectivityMonitor() {
     val connectivityManager = getSystemService(ConnectivityManager::class.java)
@@ -328,18 +324,12 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
     }
   }
 
-  override fun stopTunnel(tunnelId: String) {
-    if (!isTunnelActive(tunnelId)) throw error("VPN profile is not active")
-    tearDownActiveTunnel()
-  }
-
-  override fun isTunnelActive(tunnelId: String): Boolean = tunnelConfig?.id == tunnelId
-
   private inner class NetworkConnectivityMonitor : NetworkCallback() {
 
     override fun onAvailable(network: Network) {
-      broadcastVpnConnectivityChange(TunnelStatus.Connected)
-      updateNotification(TunnelStatus.Connected)
+      val tunnelId = tunnelConfig?.id ?: ""
+      val status = TunnelStatus.Connected(tunnelId)
+      updateNotification(status)
 
       val isUdpSupported = remoteDevice?.updateUDPSupport() == true
       // Why do we need to persist this? TODO(fortuna): remove.
@@ -347,24 +337,16 @@ class VpnTunnelServiceImpl : VpnTunnelService, VpnService() {
     }
 
     override fun onLost(network: Network) {
-      broadcastVpnConnectivityChange(TunnelStatus.Reconnecting)
-      updateNotification(TunnelStatus.Reconnecting)
+      val tunnelId = tunnelConfig?.id ?: ""
+      val status = TunnelStatus.Reconnecting(tunnelId)
+      updateNotification(status)
     }
   }
 
   companion object {
-    private val Log: Logger = Logger.getLogger(VpnTunnelServiceImpl::class.java.getName())
     private const val NotificationServiceId: Int = 1
     private const val NotificationColor: Int = 0x00BFA5
     private const val NotificationChannelID: String = "outline-vpn"
-    private val DnsResolverIPAddresses: Array<String> = arrayOf<String>(
-      "208.67.222.222", // OpenDNS
-      "208.67.220.220", // OpenDNS
-      "1.1.1.1", // Cloudflare
-      "9.9.9.9", // Quad9
-    )
-
-    const val StatusBroadcastKey: String = "onStatusChange"
   }
 
   private data class Subnet(var address: String, var prefix: Int) {
--- core/network/src/androidMain/kotlin/com/bobarik/konspekt/core/network/models/TunnelStatus.kt
+++ core/network/src/androidMain/kotlin/com/bobarik/konspekt/core/network/models/TunnelStatus.kt
@@ -2,10 +2,20 @@ package com.bobarik.konspekt.core.network.models
 
 import androidx.annotation.StringRes
 import com.bobarik.konspekt.core.network.R
+import kotlinx.serialization.Serializable
 
-enum class TunnelStatus(val value: Int, @StringRes val labelId: Int) {
-  Invalid(-1, R.string.notification_state_reconnecting),
-  Connected(0, R.string.notification_state_connected),
-  Disconnected(1, R.string.notification_state_reconnecting),
-  Reconnecting(2, R.string.notification_state_reconnecting);
+@Serializable
+sealed class TunnelStatus(val value: Int, @StringRes val labelId: Int) {
+
+  @Serializable
+  data object Invalid : TunnelStatus(-1, R.string.notification_state_reconnecting)
+
+  @Serializable
+  data class Connected(val id: String) : TunnelStatus(0, R.string.notification_state_connected)
+
+  @Serializable
+  data object Disconnected : TunnelStatus(1, R.string.notification_state_reconnecting)
+
+  @Serializable
+  data class Reconnecting(val id: String) : TunnelStatus(2, R.string.notification_state_reconnecting)
 }
